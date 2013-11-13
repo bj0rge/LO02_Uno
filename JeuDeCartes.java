4d70493314c5ebc1c8f5b9b7ed17723f9c96589f
bj0rge <bastien.jorge@gmail.com>
2013-11-13 09:58:07 +0100

    Création classe JeuDeCartes & getter

--- JeuDeCartes.java
+++ JeuDeCartes.java
@@ -2,4 +2,28 @@ package LO02_Uno;
 
 public class JeuDeCartes {
 
+	// Attributs
+	private static JeuDeCartes jeu;
+	private Carte[] defausse;
+	private Carte[] pioche;
+	private boolean sensHoraire;
+	private Joueur joueurActuel;
+	
+	// Méthodes
+	private JeuDeCartes(){
+		this.sensHoraire = true;
+		// A finir
+	}
+	
+	/**
+	 * getter du singleton, crée le jeu de carte si n'existe pas
+	 * @return JeuDeCartes
+	 */
+	public static JeuDeCartes getJeuDeCartes(){
+		if (JeuDeCartes.jeu == null ){
+			jeu = new JeuDeCartes();
+		}
+		return JeuDeCartes.jeu;				
+	}
+	
 }

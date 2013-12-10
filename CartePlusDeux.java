cd7f9735da228d142030e63cbda99acc12674f3a
Bjorge <bastien.jorge@gmail.com>
2013-12-11 00:02:55 +0100

    Correction CartePlusDeux. Anciennement syso(Manche.getInstance().getJoueurActuel()), mais ça ne retourne pas le numéro. Il aurait fallu implémenter un toString sur Joueur. Ajout de la fonction estJouable(Carte)

--- CartePlusDeux.java
+++ CartePlusDeux.java
@@ -10,15 +10,25 @@ public class CartePlusDeux extends Carte {
 	
 	public void appliquerEffets(){
 		
-		System.out.println("Le joueur " + Manche.getInstance().getJoueurActuel() + "a posé un +2 !");
-		System.out.println("Le joueur " + Manche.getInstance().getJoueurSuivant() + "prend deux cartes et passe son tour.");
+		System.out.println("Le joueur " + (Partie.getInstance().getListeJoueurs().indexOf(Manche.getInstance().getJoueurSuivant()) + 1) + " pioche :");
 		
 		for (int i = 0; i < 2; i++) {
-			Manche.getInstance().getJoueurSuivant().piocher();
+			System.out.println("Un " + Manche.getInstance().getJoueurSuivant().piocher());
 		}
 		Manche.getInstance().passerJoueur();
-		
+
+		System.out.println("et il passe son tour.");
 	}
+	
+	public boolean estJouable(Carte c) {
+		// Si la Carte est déjà jouable, à savoir si elle a la même couleur, c'est bon
+				boolean retour = super.estJouable(c);
+				if ((this.getClass() == c.getClass())) {
+					retour = true;
+				}
+				return retour;
+	}
+	
 	
 	public String toString() {
 		
